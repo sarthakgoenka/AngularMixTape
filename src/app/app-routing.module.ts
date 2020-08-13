0c291fff5248506fbb18e41d5001d2e3e0806123
123abc@@456 <sharthak31@gmail.com>
2020-08-13 16:34:26 +0530

    Optimize the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {PlaylistModule} from "./playlist/playlist.module";
+
 
 
 const routes: Routes = [
